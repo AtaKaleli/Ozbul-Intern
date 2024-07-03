a6c69af0e7d351bc3ff617cf9ed06dbf60e33eef
ata kaleli <atakaleli123@gmail.com>
2024-07-03 08:48:44 +0300

    Worker Updated

--- WebApplication1/Models/Worker.cs
+++ WebApplication1/Models/Worker.cs
@@ -4,17 +4,14 @@ namespace WebApplication1.Models
 {
     public class Worker
     {
-        public int ID { get; set; }
-        [Required]
+        public int Id { get; set; }
+       
         public string FirstName { get; set; }
-        [Required]
+        
         public string LastName { get; set; }
         public Role Role { get; set; }
 
     }
 
-    public enum Role
-    {
-        Director,Actor
-    }
+    
 }
