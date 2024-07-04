02b98894c22e6b43fd8bf85b0285d6d58bd7d71b
ata kaleli <atakaleli123@gmail.com>
2024-07-04 11:27:53 +0300

    Update MovieDbContext.cs

--- WebApplication1/Data/MovieDbContext.cs
+++ WebApplication1/Data/MovieDbContext.cs
@@ -10,6 +10,40 @@ namespace WebApplication1.Data
 
         }
 
-        
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Theatre> Theatres { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+        public DbSet<MovieTheatre> MovieTheatres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MovieActor>()
+                .HasKey(ma => new { ma.MovieId, ma.ActorId });
+            modelBuilder.Entity<MovieActor>()
+                .HasOne(m => m.Movie)
+                .WithMany(ma => ma.MovieActors)
+                .HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<MovieActor>()
+                .HasOne(m => m.Actor)
+                .WithMany(ma => ma.MovieActors)
+                .HasForeignKey(m => m.ActorId);
+
+            modelBuilder.Entity<MovieTheatre>()
+                .HasKey(mt => new { mt.MovieId, mt.TheatreId });
+            modelBuilder.Entity<MovieTheatre>()
+                .HasOne(m => m.Movie)
+                .WithMany(ma => ma.MovieTheatres)
+                .HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<MovieTheatre>()
+                .HasOne(m => m.Theatre)
+                .WithMany(mt => mt.MovieTheatres)
+                .HasForeignKey(m => m.TheatreId);
+
+
+
+        }
+
+
     }
 }
