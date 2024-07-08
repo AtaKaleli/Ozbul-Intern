a678d2bb9d97429acb35b307c48e886899e17f6d
ata kaleli <atakaleli123@gmail.com>
2024-07-08 11:09:46 +0300

    Movie Interface, Repository and Controller is added

---
+++ WebApplication1/Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController:Controller
+    {
+        private readonly IMovieRepository _movieRepository;
+        public MovieController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(200, Type=typeof(IEnumerable<Movie>))]
+        public IActionResult GetMovies()
+        {
+            var movies = _movieRepository.GetMovies();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(movies);
+        }
+    }
+}
---
+++ WebApplication1/Repositories/MovieRepository.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication1.Data;
+using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repositories
+{
+    public class MovieRepository:IMovieRepository
+    {
+        private readonly MovieDbContext _context;
+        public MovieRepository(MovieDbContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Movie> GetMovies()
+        {
+            return _context.Movies.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
