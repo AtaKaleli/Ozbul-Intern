694d2d394042dc25c4b5651fc3b45f70926a0d24
ata kaleli <atakaleli123@gmail.com>
2024-07-15 12:32:47 +0300

    ID is added to Update dto s

--- WebApplication1/Dto/UpdateMovieDto.cs
+++ WebApplication1/Dto/UpdateMovieDto.cs
@@ -4,6 +4,7 @@ namespace WebApplication1.Dto
 {
     public class UpdateMovieDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
         public int DirectorId { get; set; }
--- WebApplication1/Dto/UpdateTheatreDto.cs
+++ WebApplication1/Dto/UpdateTheatreDto.cs
@@ -4,7 +4,7 @@ namespace WebApplication1.Dto
 {
     public class UpdateTheatreDto
     {
-        
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
     }
